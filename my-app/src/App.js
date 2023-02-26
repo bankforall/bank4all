e061e6e04c892fa23a7783a328b9dac81ad0fdbd
Kridtin Sanardhan <kridtin.san@ascendcorp.com>
2023-02-26 18:07:34 +0700

    init login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./dashboard/dashboard";
 import Login from "./login/login";
+import UserInformation from "./userInformation/userInformation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Dashboard></Dashboard>
-        <Login></Login>
+        <UserInformation></UserInformation>
       </header>
+      <Dashboard></Dashboard>
     </div>
   );
 }
---
+++ my-app/src/userInformation/userInformation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './userInformation.css';
+
+class Login extends React.Component {
+
+  render() {
+    return (
+      <div className={"user-container"}>
+        <div> Logo </div>
+        <div> Username </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
