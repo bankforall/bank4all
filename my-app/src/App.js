008f3966802529a1e71df0695658259f22878375
Kridtin Sanardhan <kridtin.san@ascendcorp.com>
2023-02-26 17:21:09 +0700

    change to react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import dashboard from './dashboard/dashboard';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <h1>asdf</h1>
+        <dashboard />
       </header>
     </div>
   );
